afd9bcd4a986eacfbb5089ba844ffab8abb5f471
SakkeEU <sakke.eu@gmx.com>
2020-07-20 20:55:04 +0200

    add syncd_send() and send_cd() functions

--- components/include/syncd_connection.h
+++ components/include/syncd_connection.h
@@ -4,11 +4,26 @@
 #include <stdint.h>
 
 #define TAG_WIFI "SYNCD_WIFI"
+#define TAG_ESPNOW "SYNCD_ESPNOW"
 
 #define ESPNOW_CHANNEL 1
 
-void syncd_wifi_init();
+typedef enum{
+	SEND_FAIL,
+	SEND_SUCCESS,
+	SEND_NOT_FINISHED
+}syncd_send_result_t;
+
+typedef struct{
+	uint8_t * buf;
+	uint8_t len;
+}syncd_packet_t;
+
+void syncd_send(void * param);
+
 void syncd_espnow_init();
 void syncd_espnow_deinit();
+void syncd_wifi_init();
+void syncd_wifi_deinit();
 
 #endif
